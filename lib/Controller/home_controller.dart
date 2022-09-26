60037d4f828b4b524d316797eb20f827322da56f
Gabriel Gamosi Silva <gamosi.dev@gmail.com>
2022-09-26 15:12:47 -0300

    alteração do local do metodo de logout

--- lib/Controller/home_controller.dart
+++ lib/Controller/home_controller.dart
@@ -3,8 +3,4 @@ import 'package:instacopy2/Controller/firebase_database_controller.dart';
 class HomeController {
   final FirebaseDatabaseController _firebaseDatabaseController =
       FirebaseDatabaseController();
-
-  Future<void> logoutUser() async {
-    await _firebaseDatabaseController.signOutFromLoggedUser();
-  }
 }
