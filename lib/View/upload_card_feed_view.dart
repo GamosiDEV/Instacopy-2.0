bc9298253f4b89dd304b0e570d5807d91fe8ea2a
Gabriel Gamosi Silva <gamosi.dev@gmail.com>
2022-11-16 15:09:19 -0300

    Criação da tela de feed de postagens do usuario

---
+++ lib/View/upload_card_feed_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class UploadCardFeedView extends StatefulWidget {
+  const UploadCardFeedView({Key? key}) : super(key: key);
+
+  @override
+  State<UploadCardFeedView> createState() => _UploadCardFeedViewState();
+}
+
+class _UploadCardFeedViewState extends State<UploadCardFeedView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/View/user_upload_feed_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class UserUploadFeedView extends StatefulWidget {
+  const UserUploadFeedView({Key? key}) : super(key: key);
+
+  @override
+  State<UserUploadFeedView> createState() => _UserUploadFeedViewState();
+}
+
+class _UserUploadFeedViewState extends State<UserUploadFeedView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
